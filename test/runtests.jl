a7bb1082d8409931b31026c6929da97e0a00b6fa
Ulrich Thiel <6717247+ulthiel@users.noreply.github.com>
2026-08-09 18:00:00 +0200

    Add FS2 regression tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,21 @@
-using CharacterLab
+using FS2
+using Oscar
 using Test
 
-@testset "CharacterLab.jl" begin
-    # Write your tests here.
+@testset "FS2.jl" begin
+    @testset "Known FS2 failures" begin
+        T = character_table("Co2")
+        res, failures = is_fs2_with_data(T; q=2, findall=true)
+
+        @test !res
+        @test length(failures) == 1
+        @test only(failures).α_label == raw"\chi_{11}"
+
+        G = small_group(189, 5)
+        T = character_table(G)
+        res, failures = is_fs2_with_data(T; q=3, findall=true)
+
+        @test !res
+        @test !isempty(failures)
+    end
 end
