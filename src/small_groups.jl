30685ef83ec9d4c63c339c276a2eddcfe12283b3
Ulrich Thiel <6717247+ulthiel@users.noreply.github.com>
2026-08-24 09:28:54 +0200

    Use ZZ initializer for SmallGroups total

--- src/small_groups.jl
+++ src/small_groups.jl
@@ -5,5 +5,5 @@ Return the total number of groups in the GAP SmallGroups library whose orders
 lie in `orders`.
 """
 function total_number_of_small_groups(orders::AbstractVector{<:Integer})
-    return sum(number_of_small_groups, orders; init=0)
+    return sum(number_of_small_groups, orders; init=ZZ(0))
 end
