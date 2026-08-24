94cb5498623d166c6b33363748184e7d33546931
Ulrich Thiel <6717247+ulthiel@users.noreply.github.com>
2026-08-24 09:25:15 +0200

    Avoid type piracy in helper functions

--- src/small_groups.jl
+++ src/small_groups.jl
@@ -1,9 +1,9 @@
 """
-    number_of_small_groups(orders::AbstractVector{<:Integer})
+    total_number_of_small_groups(orders::AbstractVector{<:Integer})
 
 Return the total number of groups in the GAP SmallGroups library whose orders
 lie in `orders`.
 """
-function number_of_small_groups(orders::AbstractVector{<:Integer})
+function total_number_of_small_groups(orders::AbstractVector{<:Integer})
     return sum(number_of_small_groups, orders; init=0)
 end
