ce2291bca12cff661b8c4e3d26292cc067682e61
Ulrich Thiel <6717247+ulthiel@users.noreply.github.com>
2026-08-24 09:13:22 +0200

    Generalize SmallGroups count helper

--- src/small_groups.jl
+++ src/small_groups.jl
@@ -1,9 +1,9 @@
 """
-    number_of_small_groups(orders::AbstractUnitRange{<:Integer})
+    number_of_small_groups(orders::AbstractVector{<:Integer})
 
 Return the total number of groups in the GAP SmallGroups library whose orders
 lie in `orders`.
 """
-function number_of_small_groups(orders::AbstractUnitRange{<:Integer})
+function number_of_small_groups(orders::AbstractVector{<:Integer})
     return sum(number_of_small_groups, orders; init=0)
 end
