2a6ac56727776f9c601e29ba306f78e5ebef2072
Ulrich Thiel <6717247+ulthiel@users.noreply.github.com>
2026-08-24 09:12:15 +0200

    Add SmallGroups range utilities

---
+++ src/small_groups.jl
@@ -0,0 +1,9 @@
+"""
+    number_of_small_groups(orders::AbstractUnitRange{<:Integer})
+
+Return the total number of groups in the GAP SmallGroups library whose orders
+lie in `orders`.
+"""
+function number_of_small_groups(orders::AbstractUnitRange{<:Integer})
+    return sum(number_of_small_groups, orders; init=0)
+end
